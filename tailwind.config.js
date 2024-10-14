0102a5abe9793b040de546d28ca82cda95ea62ec
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2024-10-14 15:37:45 +0000

    Setup Phlex

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   content: [
+    './app/views/**/*.rb', // Phlex views
+    './app/components/**/*.rb', // Phlex components
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
