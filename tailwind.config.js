1e132752eeaf9927d22ec7c8964574a370f16ebf
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2024-10-15 06:13:42 +0000

    Add code and UI fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,5 +6,44 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+  theme: {
+    fontFamily: {
+      'cascadia-mono': ['CascadiaMono', 'monospace'],
+      body: [
+        'NotoNaskhArabicUI',
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        'Segoe UI',
+        'Roboto',
+        'Helvetica Neue',
+        'Arial',
+        'Noto Sans',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Color Emoji'
+      ],
+      sans: [
+        'NotoNaskhArabicUI',
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'system-ui',
+        'Segoe UI',
+        'Roboto',
+        'Helvetica Neue',
+        'Arial',
+        'Noto Sans',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Color Emoji'
+      ]
+    },
+  }
 }
