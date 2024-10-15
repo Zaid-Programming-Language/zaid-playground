cdca3574ac76f289daff0b57cede9448b8abf19a
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2024-10-15 20:38:18 +0000

    Change playground layout

--- app/views/pages/index.rb
+++ app/views/pages/index.rb
@@ -3,11 +3,15 @@
 class Views::Pages::Index < Views::Base
   def view_template
     Layout(title: t("zaid_playground")) do
-      div(data: { controller: "codemirror", codemirror_doc_value: "" }) do
-        div(class: "[&_*]:font-cascadia-mono [&_.cm-editor]:h-[50vh]", data: { codemirror_target: "editor" })
-      end
+      div(class: "flex flex-col sm:flex-row h-screen") do
+        div(class: "flex flex-1", data: { controller: "codemirror", codemirror_doc_value: "" }) do
+          div(
+            class: "[&_*]:font-cascadia-mono w-full [&_.cm-editor]:h-[50vh] sm:[&_.cm-editor]:h-screen",
+            data: { codemirror_target: "editor" }
+          )
+        end
 
-      div(class: "h-[50vh]") do
+        div(class: "flex flex-1")
       end
     end
   end
