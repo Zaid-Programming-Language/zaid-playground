620679334231548b441375ff94978807562a68b4
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2024-10-18 19:05:37 +0000

    Use curl instead of wget

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,3 +1,3 @@
 bin/setup --skip-server
 
-wget -O app/javascript/controllers/zaid.wasm https://files.zaid-lang.org/zaid.wasm
+curl -L -o app/javascript/controllers/zaid.wasm https://files.zaid-lang.org/zaid.wasm
